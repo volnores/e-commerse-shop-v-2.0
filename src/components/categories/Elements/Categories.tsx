e6b409c6de7850b19371228f3a8f63dcf3748169
volnores <volnores2409@mail.ru>
2024-10-23 21:07:08 +0300

    add categories

--- src/components/categories/Elements/Categories.tsx
+++ src/components/categories/Elements/Categories.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 const Categories: React.FC = () => {
   return (
-    <ul className="grid gap-16 grid-rows-4 grid-cols-2 w-full xl:max-w-[1750px] mx-auto mt-6">
-      <li className="bg-blue-200 row-span-2">rererere</li>
-      <li className="bg-yellow-200 col-span-2">rererere</li>
-      <li className="bg-green-200 col-span-2">rererere</li>
-      <li className="bg-red-200 col-span-3">rererere</li>
-      <li className="bg-purple-200 col-span-2">rererere</li>
-      <li className="bg-sky-200 row-span-1">rererere</li>
+    <ul className="grid grid-cols-3 gap-x-2 gap-y-2 grid-flow-row-dense md:mt-8 w-full xl:max-w-[1750px] mx-auto">
+      <li className="bg-blue-200 col-span-1 row-span-1 min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+      <li className="bg-yellow-200 col-span-2 row-span-1  min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+      <li className="bg-green-200 col-span-2 row-span-2 min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+      <li className="bg-red-200 row-span-3 min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+      <li className="bg-purple-200  min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+      <li className="bg-sky-200  min-h-[50px] px-16 py-16 rounded-sm">rererere</li>
+     
     </ul>
   );
 };
 
 export default Categories;
-// hidden lg:grid  gap-4 grid-rows-9 grid-cols-2 md:grid-cols-9 w-full xl:max-w-[2100px] mx-auto
+
--- src/components/categories/GridCategories/index.tsx
+++ src/components/categories/GridCategories/index.tsx
@@ -5,7 +5,6 @@ const index: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center md: mt-8 w-full max-w-[1750px] mx-auto rounded-sm">
       <h1 className="text-3xl font-bold">Product categories</h1>
-     
       <Categories />
     </div>
   );
