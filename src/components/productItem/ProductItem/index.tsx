7c8d3b042f04d723f40bf2cdb22370c798192561
volnores <volnores2409@mail.ru>
2024-10-25 23:36:35 +0300

    add style itemsList

--- src/components/productItem/ProductItem/index.tsx
+++ src/components/productItem/ProductItem/index.tsx
@@ -1,14 +1,20 @@
 
 import { ProductItem } from '../../../interfaces/productItem'
+import { SearchOutlined } from '@ant-design/icons'
 
 
 const index = ({...item}: ProductItem) => {
-    const {brand, price, image, title, category, description, color, id, model, popular, discount} = item
+    const { price, image, model, discount} = item
 
   return (
-    <div className='border-2'>
-      <h1>{brand}</h1>
-      <img src={image} alt="img" className='h-64'/>
+    <div className='border-2 px-4 py-2 rounded-sm bg-gray-50 relative'>
+      <img src={image} alt="img" className='h-64 w-64 rounded-sm gap-6 mt-4 cursor-pointer'/>
+      <div className='absolute right-6 top-8 p-2 rounded-full bg-slate-100 hover:bg-slate-200 flex cursor-pointer'>{<SearchOutlined/>}</div>
+   <div className='flex  py-4 text-xl'>{model.slice(0, 25)}</div>
+    <div className='flex items-center justify-between'>
+    <div className='flex justify-center py-2 text-lg font-semibold'>{`$${price}`}</div>
+    <button className='bg-gray-100 px-2 py-2 rounded-sm hover:bg-gray-200'>Add to Cart</button>
+    </div>
       </div>
   )
 }
